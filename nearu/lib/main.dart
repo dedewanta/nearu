d5fc6bca7858937d8f9f0c7b16b902982dd5e9f5
Adrian463588 <adriansyah2020@mail.ugm.ac.id>
2022-03-20 17:01:26 +0700

    pengerjaan 1

---
+++ nearu/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'package:nearu/app/widgets/splash.dart';
+import 'app/routes/app_pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Application",
+      initialRoute: Routes.LOGIN,
+      getPages: AppPages.routes,
+    );
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SplashScreen();
+        } else {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Application",
+            initialRoute: Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }
+      },
+    );
+  }
+}
